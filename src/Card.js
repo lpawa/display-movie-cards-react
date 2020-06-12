c6b218cd68763f9b1e3c3a9e2b1b5994acf0f15c
lpawa <lakshyapawa110@gmail.com>
2020-06-13 04:38:44 +0530

    add alt text

--- src/Card.js
+++ src/Card.js
@@ -12,7 +12,7 @@ class Card extends React.Component {
             const MOVIE_CARD_ID = "movie-card-" + movie.movie_id;
             return (
                 <div className="card" id={MOVIE_CARD_ID} key={MOVIE_CARD_ID}>
-                    <img className={"card-image"} src={movie.movie_poster}/>
+                    <img className={"card-image"} src={movie.movie_poster} alt={movie.movie_name}/>
                     <div className="card-block">
                         <h4 className="card-title">{movie.movie_name}</h4>
                         <p className="card-text">{movie.movie_overview}</p>
