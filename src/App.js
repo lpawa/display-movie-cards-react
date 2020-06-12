48e4dca6786d919bf8977bc4053326f4a3650c59
lpawa <lakshyapawa110@gmail.com>
2020-06-13 04:35:22 +0530

    Show movie cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './movieData';
+import Card from './Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        data: []
+    };
+  }
+  componentDidMount() {
+      this.setState({
+          data: data
+      })
+  }
+  render() {
+      return (
+          <div className="App">
+              <header className="App-header">
+                  <p>
+                      Movies in Bangalore
+                  </p>
+                  {
+                      this.state.data && this.state.data.length ? <div className={"card-container"}>
+                          {
+                              this.state.data.map(function (movie, index) {
+                                  return <Card movie={movie} index={index} key={index}/>
+                              })
+                          }
+                      </div> : null
+                  }
+              </header>
+
+          </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+class Card extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        if(this.props.movie) {
+            let movie = this.props.movie;
+            const MOVIE_CARD_ID = "movie-card-" + movie.movie_id;
+            return (
+                <div className="card" id={MOVIE_CARD_ID} key={MOVIE_CARD_ID}>
+                    <img className={"card-image"} src={movie.movie_poster}/>
+                    <div className="card-block">
+                        <h4 className="card-title">{movie.movie_name}</h4>
+                        <p className="card-text">{movie.movie_overview}</p>
+                    </div>
+                </div>
+            );
+        } else {
+            return <div>Empty Card</div>
+        }
+    }
+}
+
+export default Card;
